a661308eab6818c6b842100d86afb8e25a5fd2a7
Tedmaloney <Tedmaloney@me.com>
2016-11-19 17:29:19 -0500

    Working login with sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "players#index"
   resources :players
-  resources :users, only: %i(new)
+  resources :users, only: %i(new create)
   get '/brackets' => 'players#brackets'
 
 end
