8fdfd900683d632172ca033f08a33f7ace854b12
Tedmaloney <Tedmaloney@me.com>
2016-11-19 17:17:15 -0500

    sessions controller added basic methods, create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,27 @@
 class SessionsController < ApplicationController
 
 
-  def create
-    user = User.find_by()
+  def create # Allows the user to log in (create a session)
+    user = User.find_by(email: allowed_session_params[:email])
+    if user && user.authenticate(allowed_session_params[:password])
+      session[:user_id] = user
+      redirect_to root_url
+    else
+      redirect_to :back
+    end
+  end
+
+  def destroy # allows the user to log out, (set the session to nil)
+    session[:user_id] = nil
+    redirect_to root_url
+  end
+
+
+
+
+
+  private
+  def allowed_session_params
+    params.require(:session).permit(:email, :password)
   end
 end
