642870ebbb359e27af894fae276f24fcc911ecb3
Tedmaloney <Tedmaloney@me.com>
2016-11-19 17:18:38 -0500

    Added current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user #Finds the current user if it exists
+    if User.all.count > 0
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  end
+
+  helper_method :current_user #Allows the method to be called anywhere
 end
