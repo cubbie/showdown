522d8cd457065d154320058ecaf32be76df18a55
Tedmaloney <Tedmaloney@me.com>
2016-11-19 17:12:43 -0500

    Added basic user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(allowed_params)
+    if @user.save
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
 
   private
 
-  
+  def allowed_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
