8bdc2eb3a71410e4a6b465111623dae2b68ed971
Tedmaloney <Tedmaloney@me.com>
2016-11-21 00:44:14 -0500

    Adding joining and removing brackets, also added a reference from the user to the bracker

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -22,6 +22,21 @@ class BracketsController < ApplicationController
     @bracket = Bracket.find_by(show_bracket_params)
   end
 
+  def process_status
+
+
+    bracket = Bracket.find_by(id: process_request_params[:bracket_id])
+    user = User.find_by(id: process_request_params[:user_id])
+    if process_request_params[:status] == "remove"
+      leave_bracket(bracket, user)
+    elsif process_request_params[:status] == "join"
+      join_bracket(bracket, user)
+    end
+    redirect_to root_url
+  end
+
+
+
 
 
 private
@@ -32,4 +47,19 @@ private
   def show_bracket_params
     params.permit(:id)
   end
+
+  def process_request_params
+    params.require(:information).permit(:status, :bracket_id, :user_id)
+  end
+
+  def leave_bracket(bracket, user)
+    bracket.users.delete(user)
+    bracket.save
+  end
+
+  def join_bracket(bracket, user)
+    bracket.users << user
+    bracket.save
+  end
+  helper_method :add_user_to_bracket, :remove_user_from_bracket
 end
--- app/models/bracket.rb
+++ app/models/bracket.rb
@@ -1,7 +1,10 @@
 class Bracket < ApplicationRecord
+  has_many :users
 
   def info
     return ["#{self.game_name}", "#{self.description}", "#{self.admin}"]
   end
+
+
 
 end
