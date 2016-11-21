9312aa7aa4a3c0541a46349854170c83731c31aa
Tedmaloney <Tedmaloney@me.com>
2016-11-20 22:08:53 -0500

    Added seeds for tournaments, added list of all tournaments on index

--- app/models/bracket.rb
+++ app/models/bracket.rb
@@ -1,4 +1,7 @@
 class Bracket < ApplicationRecord
-  
+
+  def info
+    return ["#{self.game_name}", "#{self.description}", "#{self.admin}"]
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root "players#index"
   resources :players
   resources :users, only: %i(new create)
-  get '/brackets' => 'players#brackets'
   resources :sessions, only: %i(new create destroy)
   resources :brackets, only: %i(index new create show)
 end
