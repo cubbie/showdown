4c2ba8a64ec3ba45829573379a0715adbcd9f612
Tedmaloney <Tedmaloney@me.com>
2016-11-28 15:06:31 -0500

    Fixed turbolinks

--- app/assets/javascripts/newbracket.js
+++ app/assets/javascripts/newbracket.js
@@ -1,8 +1,6 @@
 $(function() {
-  console.log(window.location.pathname)
-  console.log("/")
 
-  if (window.location.pathname === '/') {
+  if (window.location.pathname === '/' ||window.location.pathname === "" ) {
     $('.nav').css('display', 'none');
     $('.page-container').css('margin-left', '0');
   }
