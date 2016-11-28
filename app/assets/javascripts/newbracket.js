e221cc6c461977c202158db2add0bd1080b42f5a
Tedmaloney <Tedmaloney@me.com>
2016-11-28 14:50:29 -0500

    Fixed root url nav hide

--- app/assets/javascripts/newbracket.js
+++ app/assets/javascripts/newbracket.js
@@ -1,6 +1,8 @@
 $(function() {
+  console.log(window.location.pathname)
+  console.log("/")
 
-  if (window.location.pathname === '<%= root_path %>') {
+  if (window.location.pathname === '/') {
     $('.nav').css('display', 'none');
     $('.page-container').css('margin-left', '0');
   }
